16fe48a217a055afd0d282249dec893e86ee9e2d
igorleitezup <83731128+igorleitezup@users.noreply.github.com>
2021-09-15 21:24:26 -0300

    first commit

---
+++ app/src/main/java/com/example/themooviebd/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.themooviebd.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.themooviebd.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
